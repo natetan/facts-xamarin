81feeda895fb86d148d8dce3c4fb99435b174405
Yulong Tan <yulongtan67@gmail.com>
2017-06-08 00:54:26 -0700

    add initial fact getter

--- FunFactivity/MainActivity.cs
+++ FunFactivity/MainActivity.cs
@@ -5,7 +5,8 @@ using Android.OS;
 namespace FunFactivity {
 	[Activity(Label = "FunFactivity", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity {
-		int count = 1;
+
+		FactBook factBook = new FactBook();
 
 		protected override void OnCreate(Bundle savedInstanceState) {
 			base.OnCreate(savedInstanceState);
@@ -13,7 +14,12 @@ namespace FunFactivity {
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
+			TextView factTextView = FindViewById<TextView>(Resource.Id.factTextView);
+			Button showFactButton = FindViewById<Button>(Resource.Id.showFactButton);
 
+			showFactButton.Click += (sender, e) => {
+				factTextView.Text = factBook.getFact(0);
+			};
 
 		}
 	}
